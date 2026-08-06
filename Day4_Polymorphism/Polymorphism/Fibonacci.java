0532c1293bf075ee039e12ede0e826bda239ebb1
Abhishek Yadav <abhishek.j.yadav10@gmail.com>
2026-08-06 23:40:44 +0530

    Added Fibonacci example

---
+++ Day4_Polymorphism/Polymorphism/Fibonacci.java
@@ -0,0 +1,33 @@
+package Polymorphism;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    // Function to print Fibonacci series
+    public static void fibonacci(int n) {
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        fibonacci(n);
+
+        sc.close();
+    }
+}
